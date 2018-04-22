9eaabb12e614827d360219636d481cbc3e5aa2bd
Jason <parkjs814@gmail.com>
2018-04-22 09:30:02 -0500

    awegweg

--- built/main.8d477758e0e3f07c8390.js
+++ built/main.8d477758e0e3f07c8390.js
@@ -109374,7 +109374,7 @@ var Camera = function (_React$Component) {
           };
           var data = new FormData();
           data.append('file', dataURLtoBlob(imageSrc));
-          _axios2.default.post('http://206.189.178.156/validate-image', data, {
+          _axios2.default.post('https://litter-bug.com/validate-image', data, {
             headers: {
               'accept': 'application/json',
               'Accept-Language': 'en-US,en;q=0.8',
--- src/components/Camera/index.jsx
+++ src/components/Camera/index.jsx
@@ -28,7 +28,7 @@ class Camera extends React.Component {
         };
         const data = new FormData();
         data.append('file', dataURLtoBlob(imageSrc));
-        axios.post('http://206.189.178.156/validate-image', data, {
+        axios.post('https://litter-bug.com/validate-image', data, {
           headers: {
             'accept': 'application/json',
             'Accept-Language': 'en-US,en;q=0.8',
